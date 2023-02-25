e5fa7f131df368f54b6d3f23507e9b903bb2d659
arigunawan12 <ts.arigunawan@gmail.com>
2023-02-26 01:15:04 +0700

    start my project

---
+++ src/components/Logout/Logout.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+function Logout() {
+  const isLoggedIn = Boolean(localStorage.getItem("token"));
+
+  const handleLogout = () => {
+    axios
+      .get(`${process.env.REACT_APP_BASEURL}api/v1/logout`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          apiKey: `${process.env.REACT_APP_APIKEY}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem("token");
+        localStorage.removeItem("name");
+        localStorage.removeItem("role");
+        window.location.reload();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <div onClick={handleLogout}>Log Out</div>
+      </>
+    );
+  }
+}
+
+export default Logout;
