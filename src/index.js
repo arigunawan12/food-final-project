83c081fbb4cbce52fa960c9ff26c57c94b5619be
arigunawan12 <ts.arigunawan@gmail.com>
2023-03-11 20:17:02 +0700

    ini kenapa?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import { NavBar, Register, Login, Profile, Alluser, Home, MyFavorite, FoodBoard, FoodRating, Footer } from "./components/Index";
+import { NavBar, Register, Login, Profile, Alluser, Home, MyFavorite, FoodBoard, FoodRating, Footer } from "./components/index";
 
 const router = createBrowserRouter([
   {
